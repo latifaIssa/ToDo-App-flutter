c3ee990cf946917c058ddf5f746e02479d86d4fa
latifaIssa <latifa.masri1998@gmail.com>
2021-08-08 16:47:42 +0300

    provider test

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo/Screens/todo_list.dart';
+import 'package:todo/provider/todo_provider.dart';
 
-void main() => runApp(ToDoApp());
+void main() => runApp(
+        // Provider<TodoProvider>(
+        //   create: (context) => TodoProvider(),
+        //   child: MyApp(),
+        // ),
+        ChangeNotifierProvider(
+      create: (context) => TodoProvider(),
+      child: MyApp(),
+    ));
 
 class ToDoApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -13,6 +23,42 @@ class ToDoApp extends StatelessWidget {
         primarySwatch: Colors.cyan,
       ),
       home: TodoList(title: 'To Do List'),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To-Do List',
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                Provider.of<TodoProvider>(context, listen: false).msg,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 25),
+              ),
+              ElevatedButton(
+                //just for display ==> listen:true(default)
+                // child: Text(Provider.of<TodoProvider>(context).msg),
+                child: Text('change msg'),
+                onPressed: () {
+                  //action ==> listen:false
+                  // print(Provider.of<TodoProvider>(context, listen: false).msg);
+                  Provider.of<TodoProvider>(context, listen: false)
+                      .changeTestMsg('hello');
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
